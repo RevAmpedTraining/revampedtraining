75260bc145a27b6663a4d23bbb44c8cb9db3b432
Neil Surya <forneilu@gmail.com>
2020-08-20 19:27:10 -0700

    Hola again. Here's an update. Interesante...

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstTester_Neil.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class FirstTester_Neil extends OpMode {
+
+    private RobotRevampedSkystone robot;
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
