0bf62be7b8a7bafcb665bb5901e89f6f45ea699e
Neil Surya <forneilu@gmail.com>
2020-08-23 19:24:54 -0700

    Merge branch 'master' of https://github.com/RevAmpedTraining/revampedtraining into NeilFirstBranch
     Conflicts:
    	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest/LinearOpModeMotor_FirstAttempt_NeilSurya.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HeadingTest/FirstTester_Neil.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HeadingTest/FirstTester_Neil.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.HeadingTest;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.revAmped.components.Button;
