6ce714e858e639b5000760d025f0254405bb0464
Neil Surya <forneilu@gmail.com>
2020-08-30 15:25:54 -0700

    Merge branch 'master' of https://github.com/RevAmpedTraining/revampedtraining into NeilFirstBranch
     Conflicts:
    	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest/LinearOpModeMotor_FirstAttempt_NeilSurya.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleTeleOp/SimpleTeleOp_Neil.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleTeleOp/SimpleTeleOp_Neil.java
@@ -24,16 +24,16 @@ public class SimpleTeleOp_Neil extends OpMode {
         driveLF = hardwareMap.get(DcMotor.class, "lf");
         driveLB = hardwareMap.get(DcMotor.class, "lb");
 
-        telemetry.addLine ("Ready to start!");
+        telemetry.addLine("Ready to start!");
         telemetry.update();
     }
 
     @Override
     public void loop() {
 
-        //float x1 = gamepad1.left_stick_x;
+        float x1 = gamepad1.left_stick_x;
         float y1 = gamepad1.left_stick_y;
-        //float x2 = gamepad2.right_stick_x;
+        float x2 = gamepad2.right_stick_x;
         float y2 = gamepad2.right_stick_y;
 
         if (y1 > 0 && y2 > 0){
@@ -50,7 +50,7 @@ public class SimpleTeleOp_Neil extends OpMode {
 
             driveRF.setPower(y2);
             driveRB.setPower(y2);
-        }else if (Math.signum(y1) != Math.signum(y2) && y1 != 0 && y2 != 0){
+        } else if (Math.signum(y1) != Math.signum(y2) && y1 != 0 && y2 != 0) {
 
             driveLF.setPower(y1);
             driveLB.setPower(y1);
@@ -58,14 +58,41 @@ public class SimpleTeleOp_Neil extends OpMode {
             driveRF.setPower(y2);
             driveRB.setPower(y2);
         }
-    }
 
-    @Override
-    public void stop() {
+        if (Math.abs(x1) > 0.5f && Math.abs(x2) > 0.5f && Math.abs(y1) < 0.5f && Math.abs(y2) < 0.5f) {
 
-        driveRF.close();
-        driveLF.close();
-        driveRB.close();
-        driveLB.close();
+            if (x1 > 0 && x2 > 0) {
+
+                driveLF.setPower(x1);
+                driveLB.setPower(-x1);
+
+                driveRF.setPower(-x2);
+                driveRB.setPower(x2);
+            } else if (x1 < 0 && x2 < 0) {
+
+                driveLF.setPower(-x1);
+                driveLB.setPower(x1);
+
+                driveRF.setPower(x2);
+                driveRB.setPower(-x2);
+            } else {
+
+                driveLF.setPower(y1);
+                driveLB.setPower(y1);
+
+                driveRF.setPower(y2);
+                driveRB.setPower(y2);
+            }
+        }
+
+        telemetry.addData("Motor powers", "rf=%f, rb=%f, lf=%f, lb=%f", y2, y2, y1, y1);
+
+        {
+
+            driveRF.close();
+            driveLF.close();
+            driveRB.close();
+            driveLB.close();
+        }
     }
 }
