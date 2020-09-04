8d6ddfbb907c9eb8bd1249edb35d9990fece0571
siena-italy <cais@catlin.edu>
2020-09-03 19:21:19 -0700

    uhh

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SquareAuto/SiennaAutoSquare.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.SquareAuto;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class SiennaAutoSquare extends LinearOpMode {
+
+    private DcMotor motor;
+
+
+}
